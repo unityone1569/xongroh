1215df9e08d9a43cda620362255101d88e7d3acf
rupambora31 <iamrupam99@gmail.com>
2024-09-02 11:41:23 +0530

    project setup complete

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
